53064d7dabf733e2fa1c25d1db8dcb9f5fa7653a
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-03-27 13:27:30 -0400

    Added NetCDF and HDF5 building

--- cmake-modules/BuildHDF5.cmake
+++ cmake-modules/BuildHDF5.cmake
@@ -2,17 +2,17 @@ macro(build_hdf5 install_prefix)
 
 ExternalProject_Add(HDF5
         SOURCE_DIR HDF5
-        URL "http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.7/src/hdf5-1.8.7.tar.gz"
-        URL_MD5 "37711d4bcb72997e93d495f97c76c33a"
+        URL "http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.8/src/hdf5-1.8.8.tar.gz"
+        URL_MD5 "1196e668f5592bfb50d1de162eb16cff"
         BUILD_IN_SOURCE 1
         INSTALL_DIR     "${install_prefix}"
         BUILD_COMMAND   make 
         INSTALL_COMMAND make install 
-        CONFIGURE_COMMAND ./configure --prefix=${install_prefix}  --with-pic --disable-shared --disable-cxx --disable-f77 --disable-f90 --disable-examples --disable-hl --disable-docs
+        CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-pic --disable-shared --disable-cxx --disable-f77 --disable-f90 --disable-examples --disable-hl --disable-docs
       )
 
 SET(HDF5_INCLUDE_DIR ${install_prefix}/include )
 SET(HDF5_LIBRARY  ${install_prefix}/lib/libhdf5.a )
-
+SET(HDF5_FOUND ON)
 
 endmacro(build_hdf5)
--- cmake-modules/BuildNETCDF.cmake
+++ cmake-modules/BuildNETCDF.cmake
@@ -1,18 +1,18 @@
-
 macro(build_netcdf install_prefix)
 
 ExternalProject_Add(NETCDF 
   SOURCE_DIR NETCDF
-  URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.0.1.tar.gz"
-  URL_MD5 "a251453c5477599f050fa4e593295186"
+  URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.2.tar.gz"
+  URL_MD5 "b920a6c3a30e9cd46fe96d9fb65ef17e"
   BUILD_IN_SOURCE 1
   INSTALL_DIR "${install_prefix}"
   BUILD_COMMAND   make 
   INSTALL_COMMAND make install 
-  CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-pic --disable-netcdf4 --disable-hdf4 --disable-dap --disable-shared --disable-cxx --disable-f77 --disable-f90 --disable-examples --enable-v2 --disable-docs
+  CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-pic --disable-doxygen --disable-hdf4 --disable-netcdf-4 --disable-shared --disable-dap
 )
 
 SET(NETCDF_LIBRARY ${install_prefix}/lib/libnetcdf.a )
 SET(NETCDF_INCLUDE_DIR ${install_prefix}/include )
+SET(NETCDF_FOUND ON)
 
 endmacro(build_netcdf)
