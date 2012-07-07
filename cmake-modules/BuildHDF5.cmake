032604f117871d3ca3eddcd53d64034dfca46bae
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-07-07 14:55:01 -0400

    Fixed bug in HDF5 compilation, which caused it to not create deflate filter, bumped version number

--- cmake-modules/BuildHDF5.cmake
+++ cmake-modules/BuildHDF5.cmake
@@ -1,5 +1,7 @@
 macro(build_hdf5 install_prefix staging_prefix zlib_include_dir zlib_library)
 
+get_filename_component(zlib_library_dir ${zlib_library} PATH)
+
 ExternalProject_Add(HDF5
   SOURCE_DIR HDF5
   URL "http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.8/src/hdf5-1.8.8.tar.gz"
@@ -8,7 +10,7 @@ ExternalProject_Add(HDF5
   INSTALL_DIR     "${CMAKE_BINARY_DIR}/external"
   BUILD_COMMAND   make 
   INSTALL_COMMAND make DESTDIR=${CMAKE_BINARY_DIR}/external install 
-  CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-zlib=${zlib_include_dir},${zlib_library} --with-pic --disable-shared --disable-cxx --disable-f77 --disable-f90 --disable-examples --disable-hl --disable-docs
+  CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-zlib=${zlib_include_dir},${zlib_library_dir} --with-pic --disable-shared --disable-cxx --disable-f77 --disable-f90 --disable-examples --disable-hl --disable-docs
 #  INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/external
 )
 
