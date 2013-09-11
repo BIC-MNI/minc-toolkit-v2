0454cc734b6eaf23acc6e6530772de66aa8bd42f
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2013-09-11 18:26:50 -0400

    Updated NETCDF

--- cmake-modules/BuildNETCDF.cmake
+++ cmake-modules/BuildNETCDF.cmake
@@ -2,13 +2,13 @@ macro(build_netcdf install_prefix staging_prefix)
 
 ExternalProject_Add(NETCDF 
   SOURCE_DIR NETCDF
-  URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.2.1.tar.gz"
-  URL_MD5 "a8603acfd6b95bd885ef0d225c16ce9f"
+  URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.0.tar.gz"
+  URL_MD5 "39fc507bf2966980fa92defecd9d2167"
   BUILD_IN_SOURCE 1
   INSTALL_DIR     "${staging_prefix}"
   BUILD_COMMAND   make 
   INSTALL_COMMAND make DESTDIR=${staging_prefix} install 
-  CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-pic --disable-doxygen --disable-hdf4 --disable-netcdf-4 --disable-shared --disable-dap --libdir=${install_prefix}/lib${LIB_SUFFIX} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
+  CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-pic --disable-doxygen --disable-hdf4 --disable-netcdf-4 --disable-shared --disable-dap  --libdir=${install_prefix}/lib${LIB_SUFFIX} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
 #  INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/external
 )
 
