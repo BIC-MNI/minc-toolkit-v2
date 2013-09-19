a209c92ca15ecf39ab433a646a54574242944528
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2013-09-19 01:04:28 -0400

    Updated mni_autoreg and FFTW

--- cmake-modules/BuildFFTW3F.cmake
+++ cmake-modules/BuildFFTW3F.cmake
@@ -1,14 +1,20 @@
 macro(build_fftw3f install_prefix staging_prefix)
 
-ExternalProject_Add(FFTW3F
+  SET(FFTW3F_CONFIG "--enable-sse --enable-sse2  --with-pic --disable-shared --enable-threads --disable-fortran --enable-single --enable-float")
+  
+  IF(NOT APPLE)
+    SET(FFTW3F_CONFIG "${FFTW3F_CONFIG}  --enable-avx ")
+  ENDIF(NOT APPLE)
+
+  ExternalProject_Add(FFTW3F
         SOURCE_DIR FFTW3F
         URL "http://www.fftw.org/fftw-3.3.3.tar.gz"
         URL_MD5 "0a05ca9c7b3bfddc8278e7c40791a1c2"
         BUILD_IN_SOURCE 1
         INSTALL_DIR     "${staging_prefix}"
-        BUILD_COMMAND   make 
-        INSTALL_COMMAND make DESTDIR=${staging_prefix} install 
-        CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --enable-sse --enable-sse2 --enable-openmp --enable-avx --with-pic --disable-shared --enable-threads --disable-fortran --enable-single --enable-float CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
+        BUILD_COMMAND   make
+        INSTALL_COMMAND make DESTDIR=${staging_prefix} install
+        CONFIGURE_COMMAND ./configure --prefix=${install_prefix} ${FFTW3F_CONFIG} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
 #        INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/external
       )
 
