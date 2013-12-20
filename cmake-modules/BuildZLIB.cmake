e57c4ed86c7222cf58db2c4982b5a806c4ee215b
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2013-12-20 17:11:18 -0500

    Updated libminc

--- cmake-modules/BuildZLIB.cmake
+++ cmake-modules/BuildZLIB.cmake
@@ -7,21 +7,31 @@ set(ENV{CFLAGS} "${CMAKE_C_FLAGS} -fPIC")
 
 ExternalProject_Add(ZLIB
   SOURCE_DIR ZLIB
-  #URL "http://sourceforge.net/projects/libpng/files/zlib/1.2.6/zlib-1.2.6.tar.gz"
-  #URL_MD5 "618e944d7c7cd6521551e30b32322f4a"
-  URL  "http://zlib.net/zlib-1.2.8.tar.gz"
-  URL_MD5 "44d667c142d7cda120332623eab69f40"
-  BUILD_IN_SOURCE 1
-  INSTALL_DIR     "${staging_prefix}"
-  BUILD_COMMAND   make 
-  INSTALL_COMMAND make DESTDIR=${staging_prefix} install
-  CONFIGURE_COMMAND CFLAGS=-fPIC ./configure --prefix=${install_prefix} --static
+  URL  "http://www.hdfgroup.org/ftp/lib-external/CMake/ZLib.tar.gz"
+  URL_MD5 "645f5d0b7434e1f12bd0b2ae2b976f4b"
+  UPDATE_COMMAND ""
+  SOURCE_DIR ZLIB
+  BINARY_DIR ZLIB-build
+  LIST_SEPARATOR :::  
+  CMAKE_GENERATOR ${CMAKE_GEN}
+  CMAKE_ARGS
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+      -DBUILD_SHARED_LIBS:BOOL=OFF
+      -DBUILD_STATIC_EXECS:BOOL=ON
+      -DCMAKE_SKIP_RPATH:BOOL=ON
+      -DCMAKE_INSTALL_PREFIX:PATH=${install_prefix}
+      -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS} -fPIC
+      -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS} -fPIC
+      -DCMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS}  -fPIC
+      -DCMAKE_MODULE_LINKER_FLAGS=${CMAKE_MODULE_LINKER_FLAGS}  -fPIC
+      -DCMAKE_SHARED_LINKER_FLAGS=${CMAKE_SHARED_LINKER_FLAGS}  -fPIC
+  INSTALL_COMMAND make install DESTDIR=${staging_prefix}
+  INSTALL_DIR ${staging_prefix}/${install_prefix}
 )
-
 
 SET(ZLIB_INCLUDE_DIR ${staging_prefix}/${install_prefix}/include )
 SET(ZLIB_LIBRARY     ${staging_prefix}/${install_prefix}/lib/libz.a )
+SET(ZLIB_DIR         ${staging_prefix}/${install_prefix}/share/cmake/ZLIB/ )
 SET(ZLIB_FOUND ON)
-
 
 endmacro(build_zlib)
