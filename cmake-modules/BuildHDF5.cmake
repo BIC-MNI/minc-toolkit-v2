67e0641d1be8d5ffa903f86c1b6b4e2dd8a912f5
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2014-03-03 16:04:35 -0500

    Fixing build

--- cmake-modules/BuildHDF5.cmake
+++ cmake-modules/BuildHDF5.cmake
@@ -39,6 +39,7 @@ ExternalProject_Add(HDF5
 SET(HDF5_BIN_DIR     ${staging_prefix}/${install_prefix}/bin )
 SET(HDF5_INCLUDE_DIR ${staging_prefix}/${install_prefix}/include )
 SET(HDF5_LIBRARY_DIR ${staging_prefix}/${install_prefix}/lib${LIB_SUFFIX} )
+SET(HDF5_LIBRARY     ${staging_prefix}/${install_prefix}/lib${LIB_SUFFIX}/libhdf5.a )
 
 SET(HDF5_DIR         ${staging_prefix}/${install_prefix}/share/cmake/hdf5)
 SET(HDF5_FOUND ON)
