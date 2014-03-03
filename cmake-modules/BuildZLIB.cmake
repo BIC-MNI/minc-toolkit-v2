f7492667ddd1a68988f94c95c3cc9a8335d1a2bc
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2014-03-03 13:05:32 -0500

    Fixing ZLIB

--- cmake-modules/BuildZLIB.cmake
+++ cmake-modules/BuildZLIB.cmake
@@ -7,7 +7,6 @@ SET (ZLIB_VERSION_STRING 1.2)
 SET (ZLIB_VERSION_MAJOR  1.2)
 SET (ZLIB_VERSION_MINOR  8)
 
-configure_file(${CMAKE_SOURCE_DIR}/cmake-modules/ZLIB-config.cmake.install.in ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/ZLIB-config.cmake @ONLY )
 
 ExternalProject_Add(ZLIB
   URL  "http://zlib.net/zlib-1.2.8.tar.gz"
@@ -31,7 +30,6 @@ ExternalProject_Add(ZLIB
       -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
       -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
   INSTALL_COMMAND make install DESTDIR=${staging_prefix} 
-  INSTALL_COMMAND cp ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/ZLIB-config.cmake ${staging_prefix}/${install_prefix}/share/cmake/ZLIB/
   INSTALL_DIR ${staging_prefix}/${install_prefix}
 )
 
@@ -39,5 +37,9 @@ SET(ZLIB_INCLUDE_DIR ${staging_prefix}/${install_prefix}/include )
 SET(ZLIB_LIBRARY     ${staging_prefix}/${install_prefix}/lib/libz.a )
 SET(ZLIB_DIR         ${staging_prefix}/${install_prefix}/share/cmake/ZLIB/ )
 SET(ZLIB_FOUND ON)
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake-modules/ZLIB-config.cmake.install.in ${staging_prefix}/${install_prefix}/share/cmake/ZLIB/ZLIB-config.cmake @ONLY )
+
+
 
 endmacro(build_zlib)
