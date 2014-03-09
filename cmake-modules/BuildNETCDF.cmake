4ebb8cb9f6f093b2634591cbabf23a80d99226b5
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2014-03-09 13:48:35 -0400

    Updated to latest NetCDF

--- cmake-modules/BuildNETCDF.cmake
+++ cmake-modules/BuildNETCDF.cmake
@@ -2,8 +2,8 @@ macro(build_netcdf install_prefix staging_prefix)
 
 ExternalProject_Add(NETCDF 
   SOURCE_DIR NETCDF
-  URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.0.tar.gz"
-  URL_MD5 "40c0e53433fc5dc59296ee257ff4a813"
+  URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.1.1.tar.gz"
+  URL_MD5 "275c3b839088674c2f00fb3ac264bf11"
   BUILD_IN_SOURCE 1
   INSTALL_DIR     "${staging_prefix}"
   BUILD_COMMAND   make 
