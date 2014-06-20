b1a1f07acd43946f47b24d56cf3622cb928fa6c4
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2014-06-20 16:30:38 -0400

    Fixing ITKv4 patching

---
+++ cmake-modules/PatchITKv4.cmake
@@ -0,0 +1,26 @@
+message("Running PatchITKv4")
+
+macro(patch_itk_config file to_remove)
+  file(READ "${file}" config_file)
+  STRING(REPLACE "${to_remove}//" "/" config_file "${config_file}")
+  STRING(REPLACE "${to_remove}" "" config_file "${config_file}")
+  message("patched ${file} ")
+  file(WRITE "${file}" "${config_file}")
+endmacro(patch_itk_config)
+
+
+FOREACH(conf "ITKTargets-release.cmake" "Modules/ITKZLIB.cmake" "Modules/ITKZLIB.cmake" "Modules/ITKMINC.cmake" "Modules/ITKHDF5.cmake")
+  if (EXISTS "${staging_prefix}/${install_prefix}/lib/cmake/ITK-4.5/${conf}")
+    patch_itk_config("${staging_prefix}/${install_prefix}/lib/cmake/ITK-4.5/${conf}" "${staging_prefix}")
+  endif()
+ENDFOREACH(conf)
+
+
+# a hack to remove internal minc directories
+file(READ "${staging_prefix}/${install_prefix}/lib/cmake/ITK-4.5/Modules/ITKMINC.cmake" config_file)
+STRING(REPLACE ";${minc_dir}/ezminc" "" config_file "${config_file}")
+STRING(REPLACE "${CMAKE_CURRENT_BINARY_DIR}/ITKv4-build/Modules/ThirdParty/MINC/src;" "" config_file "${config_file}")
+STRING(REPLACE "${minc_dir}" "${install_prefix}/lib" config_file "${config_file}")
+STRING(REPLACE "${LIBMINC_INCLUDE_DIRS}" "${install_prefix}/include" config_file "${config_file}")
+file(WRITE "${staging_prefix}/${install_prefix}/lib/cmake/ITK-4.5/Modules/ITKMINC.cmake" "${config_file}")
+
