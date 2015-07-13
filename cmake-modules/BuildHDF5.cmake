dcff4fe8073ce97da0dc660e9cb52c74ef763f4d
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2015-07-13 16:16:15 -0400

    Updating build system

--- cmake-modules/BuildHDF5.cmake
+++ cmake-modules/BuildHDF5.cmake
@@ -24,8 +24,8 @@ macro(build_hdf5 install_prefix staging_prefix  zlib_include_dir zlib_library zl
   SET(HDF_CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG})
   SET(HDF_CMAKE_C_FLAGS_DEBUG ${CMAKE_C_FLAGS_DEBUG})
   
-  SET(HDF_CMAKE_CXX_FLAGS -fPIC ${CMAKE_CXX_FLAGS})
-  SET(HDF_CMAKE_C_FLAGS   -fPIC ${CMAKE_C_FLAGS})
+  SET(HDF_CMAKE_CXX_FLAGS "-fPIC ${CMAKE_CXX_FLAGS}")
+  SET(HDF_CMAKE_C_FLAGS   "-fPIC ${CMAKE_C_FLAGS}")
 
 ExternalProject_Add(HDF5
   URL "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.15-patch1/src/hdf5-1.8.15-patch1.tar.bz2"
