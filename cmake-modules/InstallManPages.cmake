6be52707466637a05db1f8f10e71c2eeadb1c414
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2016-03-18 15:23:42 -0400

    Switched to using pre-rendered man pages from https://github.com/BIC-MNI/man-pages/

--- cmake-modules/InstallManPages.cmake
+++ cmake-modules/InstallManPages.cmake
@@ -1,6 +1,8 @@
 # install a list of files with names like XXXX.manY into install_prefi/manY/XXXX.Y
 
 macro(INSTALL_MAN_PAGES install_prefix  )
+  
+  IF(NOT ${DISABLE_MAN_PAGES_INSTALL})
   set(argn "${ARGN}")
   
   foreach(i IN LISTS argn)
@@ -12,5 +14,5 @@ macro(INSTALL_MAN_PAGES install_prefix  )
     INSTALL(FILES ${i} DESTINATION ${install_prefix}/man${SECTION}/ RENAME ${fname}.${SECTION})
     
   endforeach()
-  
+  ENDIF(NOT ${DISABLE_MAN_PAGES_INSTALL})
 endmacro(INSTALL_MAN_PAGES )
