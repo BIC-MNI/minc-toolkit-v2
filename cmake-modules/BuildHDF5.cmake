a67245ef4143829aabd26dce762ca0d78284340a
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2016-04-04 15:30:02 -0400

    Updated to HDF 1.8.16

--- cmake-modules/BuildHDF5.cmake
+++ cmake-modules/BuildHDF5.cmake
@@ -1,4 +1,4 @@
-macro(build_hdf5 install_prefix staging_prefix  zlib_include_dir zlib_library zlib_dir)
+macro(build_hdf5 install_prefix staging_prefix)
 
   if(CMAKE_EXTRA_GENERATOR)
     set(CMAKE_GEN "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}")
@@ -27,8 +27,8 @@ macro(build_hdf5 install_prefix staging_prefix  zlib_include_dir zlib_library zl
   SET(HDF_CMAKE_C_FLAGS   "-fPIC ${CMAKE_C_FLAGS}")
   
 ExternalProject_Add(HDF5
-  URL "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.15-patch1/src/hdf5-1.8.15-patch1.tar.bz2"
-  URL_MD5 "3c0d7a8c38d1abc7b40fc12c1d5f2bb8"
+  URL "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.16/src/hdf5-1.8.16.tar.bz2"
+  URL_MD5 "79c1593573ebddf734eee8d43ecfe483"
   SOURCE_DIR HDF5
   BINARY_DIR HDF5-build
   CMAKE_GENERATOR ${CMAKE_GEN}
@@ -47,7 +47,6 @@ ExternalProject_Add(HDF5
       -DHDF5_BUILD_CPP_LIB:BOOL=ON
       -DHDF5_BUILD_TOOLS:BOOL=ON
       -DHDF5_BUILD_EXAMPLES:BOOL=OFF
-      -DHDF5_ENABLE_Z_LIB_SUPPORT=ON
       ${CMAKE_OSX_EXTERNAL_PROJECT_ARGS}
       "-DCMAKE_CXX_FLAGS_RELEASE:STRING=${HDF_CMAKE_CXX_FLAGS_RELEASE}"
       "-DCMAKE_C_FLAGS_RELEASE:STRING=${HDF_CMAKE_C_FLAGS_RELEASE}"
@@ -58,16 +57,21 @@ ExternalProject_Add(HDF5
       -DCMAKE_EXE_LINKER_FLAGS:STRING=${CMAKE_EXE_LINKER_FLAGS}
       -DCMAKE_MODULE_LINKER_FLAGS:STRING=${CMAKE_MODULE_LINKER_FLAGS}
       -DCMAKE_SHARED_LINKER_FLAGS:STRING=${CMAKE_SHARED_LINKER_FLAGS}
-#      -DZLIB_USE_EXTERNAL:BOOL=ON
-#      -DZLIB_DIR:PATH=${zlib_library_dir}
+      -DZLIB_USE_EXTERNAL:BOOL=ON
+      -DHDF5_EXTERNALLY_CONFIGURED:BOOL=ON
+      -DHDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON
+      
+      -DH5_ZLIB_HEADER:STRING=zlib.h
+      -DH5_HAVE_ZLIB_H:BOOL=ON
+      -DZLIB_INCLUDE_DIRS:STRING=${ZLIB_INCLUDE_DIR}
+      -DZLIB_INCLUDE_DIR:STRING=${ZLIB_INCLUDE_DIR}
+      -DZLIB_LIBRARIES:STRING=${ZLIB_STATIC_LIBRARY}
+      -DZLIB_STATIC_LIBRARY:STRING=${ZLIB_STATIC_LIBRARY}
+      -DZLIB_SHARED_LIBRARY:STRING=${ZLIB_SHARED_LIBRARY}
+      -DZLIB_DIR:PATH=${zlib_library_dir}
+      -DSKIP_HDF5_FORTRAN_SHARED:BOOL=ON
       -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
       -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-      -DZLIB_INCLUDE_DIRS:STRING=${zlib_include_dir}
-      -DZLIB_LIBRARIES:STRING=${zlib_library}
-      -DZLIB_INCLUDE_DIR:STRING=${zlib_include_dir}
-      -DZLIB_LIBRARY:STRING=${zlib_library}
-#      -DZLIB_DIR:PATH=${zlib_dir}
-#      -DH5_ZLIB_HEADER="zlib.h"
   INSTALL_COMMAND $(MAKE) install DESTDIR=${staging_prefix}
   INSTALL_DIR ${staging_prefix}/${install_prefix}
 #  TEST_COMMAND make test
