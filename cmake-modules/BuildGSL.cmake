df467ab1627dd0885273bfb65f4ec88e4f82531e
Vladimir Fonov <vladimir.fonov@gmail.com>
2022-12-05 15:07:09 -0500

    Merged

--- cmake-modules/BuildGSL.cmake
+++ cmake-modules/BuildGSL.cmake
@@ -1,8 +1,8 @@
 macro(build_gsl install_prefix staging_prefix)
 
   IF(CMAKE_BUILD_TYPE STREQUAL Release)
-    SET(EXT_C_FLAGS   "${CMAKE_C_FLAGS}   ${CMAKE_C_FLAGS_RELEASE}")
-    SET(EXT_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE}")
+    SET(EXT_C_FLAGS   "${CMAKE_C_FLAGS}   ${CMAKE_C_FLAGS_RELEASE} -isysroot ${CMAKE_OSX_SYSROOT}")
+    SET(EXT_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE} -isysroot ${CMAKE_OSX_SYSROOT}")
     SET(EXT_LDFLAGS   "-L${staging_prefix}/${install_prefix}/lib${LIB_SUFFIX} ${CMAKE_MODULE_LINKER_FLAGS} ${CMAKE_MODULE_LINKER_FLAGS_RELEASE}")
   ELSE()
     SET(EXT_C_FLAGS   "${CMAKE_C_FLAGS}   ${CMAKE_C_FLAGS_DEBUG}")
@@ -10,33 +10,42 @@ macro(build_gsl install_prefix staging_prefix)
     SET(EXT_LDFLAGS   "-L${staging_prefix}/${install_prefix}/lib${LIB_SUFFIX} ${CMAKE_MODULE_LINKER_FLAGS} ${CMAKE_MODULE_LINKER_FLAGS_DEBUG}" )
   ENDIF()
 
-#IF(APPLE)
-#  SET(EXT_C_FLAGS "${EXT_C_FLAGS} -isysroot ${CMAKE_OSX_SYSROOT} -std=c90")
-#  SET(EXT_CXX_FLAGS "${EXT_CXX_FLAGS} -isysroot ${CMAKE_OSX_SYSROOT}")
-#  SET(EXT_LDFLAGS "${EXT_LDFLAGS} -isysroot ${CMAKE_OSX_SYSROOT}")
-#  SET(EXT_SYSROOT "--with-sysroot=${CMAKE_OSX_SYSROOT}")
-#ELSE()
-#  SET(EXT_SYSROOT "")
-#ENDIF(APPLE)
-
-GET_PACKAGE("http://gnu.freemirror.org/gnu/gsl/gsl-2.5.tar.gz" "52fcbe147ba413d78841084cb43c95fb" "gsl-2.5.tar.gz" GSL_PATH ) 
+GET_PACKAGE("https://ftp.gnu.org/gnu/gsl/gsl-2.7.tar.gz" "9e47e81caaebcd92b7aca27a5348df74" "gsl-2.7.tar.gz" GSL_PATH ) 
 
 ExternalProject_Add(GSL
         SOURCE_DIR GSL
         URL "${GSL_PATH}"
-        URL_MD5 "52fcbe147ba413d78841084cb43c95fb"
+        URL_MD5 "9e47e81caaebcd92b7aca27a5348df74"
         BUILD_IN_SOURCE 1
         INSTALL_DIR     "${CMAKE_BINARY_DIR}/external"
         BUILD_COMMAND   $(MAKE) -s V=0 
         INSTALL_COMMAND $(MAKE) -s V=0 DESTDIR=${CMAKE_BINARY_DIR}/external install
-        CONFIGURE_COMMAND  ./configure ${EXT_SYSROOT} --enable-silent-rules --silent  --prefix=${install_prefix} --libdir=${install_prefix}/lib${LIB_SUFFIX} --with-pic --disable-shared --enable-static CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} "CXXFLAGS=${EXT_CXX_FLAGS}" "CFLAGS=${EXT_C_FLAGS}" "LDFLAGS=${EXT_LDFLAGS}"
-#        INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/external
+        CONFIGURE_COMMAND  ./configure 
+          --enable-silent-rules 
+          --silent  
+          --prefix=${install_prefix} 
+          --libdir=${install_prefix}/lib${LIB_SUFFIX} 
+          --with-pic 
+          --disable-shared 
+          --enable-static 
+          "--host=${MACHINE_NAME}"
+          "CXX=${CMAKE_CXX_COMPILER}"
+          "CC=${CMAKE_C_COMPILER}"
+          "AR=${CMAKE_C_COMPILER_AR}"
+          "RANLIB=${CMAKE_C_COMPILER_RANLIB}"
+          "NM=${CMAKE_NM}"
+          "STRIP=${CMAKE_STRIP}"
+          "CPPFLAGS=${EXT_CXX_FLAGS}"  
+          "CXXFLAGS=${EXT_CXX_FLAGS}" 
+          "CFLAGS=${EXT_C_FLAGS}" 
+          "LDFLAGS=${EXT_LDFLAGS}"
+  #        INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/external
       )
 
 SET(GSL_INCLUDE_DIR ${install_prefix}/include )
 SET(GSL_LIBRARY  ${install_prefix}/lib${LIB_SUFFIX}/libgsl.a )
 SET(GSL_CBLAS_LIBRARY ${install_prefix}/lib${LIB_SUFFIX}/libgslcblas.a )
-SET(GSL_VERSION "2.4")
+SET(GSL_VERSION "2.7")
 SET(GSL_FOUND ON)
       
 configure_file(${CMAKE_SOURCE_DIR}/cmake-modules/GSLConfig.cmake.in ${staging_prefix}/${install_prefix}/lib${LIB_SUFFIX}/GSLConfig.cmake @ONLY)
@@ -44,7 +53,7 @@ configure_file(${CMAKE_SOURCE_DIR}/cmake-modules/GSLConfig.cmake.in ${staging_pr
 SET(GSL_INCLUDE_DIR ${staging_prefix}/${install_prefix}/include )
 SET(GSL_LIBRARY  ${staging_prefix}/${install_prefix}/lib${LIB_SUFFIX}/libgsl.a )
 SET(GSL_CBLAS_LIBRARY ${staging_prefix}/${install_prefix}/lib${LIB_SUFFIX}/libgslcblas.a )
-SET(GSL_VERSION "2.4")
+SET(GSL_VERSION "2.7")
 SET(GSL_FOUND ON)
 
 endmacro(build_gsl)
